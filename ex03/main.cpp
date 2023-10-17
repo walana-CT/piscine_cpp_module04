e9d37d225e9911b25de31cb3fe938975d4a9a8b2
Walana <robin.ficht@free.fr>
2023-10-17 16:13:43 +0200

    renamed dir and added some tests

--- ex03/main.cpp
+++ ex03/main.cpp
@@ -6,7 +6,7 @@
 /*   By: rficht <robin.ficht@free.fr>               +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/10/11 16:10:32 by rficht            #+#    #+#             */
-/*   Updated: 2023/10/17 11:27:09 by rficht           ###   ########.fr       */
+/*   Updated: 2023/10/17 16:11:49 by rficht           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,7 +17,9 @@
 
 int main()
 {
-	AMateria* tmp;
+	AMateria*	ice1;
+	AMateria*	ice2;
+	AMateria*	cure1;
 
 	IMateriaSource* src = new MateriaSource();
 	src->learnMateria(new Ice());
@@ -25,18 +27,28 @@ int main()
 	src->learnMateria(new Cure());
 	src->learnMateria(new Cure());
 	src->learnMateria(new Cure());
+	
 	ICharacter* me = new Character("me");
-	tmp = src->createMateria("ice");
-	me->equip(tmp);
-	tmp = src->createMateria("cure");
-	me->equip(tmp);
-	tmp = src->createMateria("cure");
-	me->equip(tmp);
+
+	ice1 = src->createMateria("ice");
+	ice2 = src->createMateria("ice");	
+	cure1 = src->createMateria("cure");
+	
+	me->equip(ice1);
+	me->equip(cure1);
+	me->equip(ice2);
+	me->equip(ice2);
+	me->equip(ice2);
 
 	ICharacter* bob = new Character("bob");
 	me->use(0, *bob);
 	me->use(1, *bob);
-
+	me->use(5, *bob);
+	me->use(1, *bob);
+	me->unequip(1);
+	me->use(1, *bob);
+	me->equip(ice1);
+	me->use(1, *bob);
 
 	delete bob;
 	delete src;
